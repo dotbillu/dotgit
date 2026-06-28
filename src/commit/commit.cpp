d10a11f3722f3a6b8f8ef42dfcfba0b7fae6cd20
dotbillu <billudestroyer@outlook.com>
2026-06-28 17:54:25 +0530

    fixed syntax erorr

--- src/commit/commit.cpp
+++ src/commit/commit.cpp
@@ -2,12 +2,9 @@
 #include <iostream>
 
 int commit(int argc, char **argv) {
-
-  std::filesystem::rename(".dot/staged/*", ".dot/commit/*");
-
   for (const auto &entry : std::filesystem::directory_iterator(".dot/staged")) {
     std::filesystem::rename(entry.path(),
-                            ".dot/commit/" + entry.path().filename().string());
+                            ".dot/commits/" + entry.path().filename().string());
   }
   return 0;
 }
